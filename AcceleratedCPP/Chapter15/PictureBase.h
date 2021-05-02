a2bbd9cde1a6e26454965c620353ab4fc4fc5726
keikeiem <kimkm@stclab.com>
2021-05-02 22:01:00 +0900

    upload chapter15 (2) 설계

--- AcceleratedCPP/Chapter15/PictureBase.h
+++ AcceleratedCPP/Chapter15/PictureBase.h
@@ -3,7 +3,11 @@
 
 // 공통 클래스
 class PictureBase {
-private:
+	friend class FramePictureBase;
+	friend class VCatPictureBase;
+	friend class HCatPictureBase;
+	friend class StringPictureBase;
+
 	// 높이와 너비
 	typedef std::vector<std::string>::size_type ht_sz;
 	typedef std::string::size_type wd_sz;
@@ -14,42 +18,54 @@ private:
 	virtual wd_sz width() const = 0;
 	virtual ht_sz height() const = 0;
 	virtual void display(std::ostream&, ht_sz, bool) const = 0;
+public:
+	virtual ~PictureBase() {}
 };
 
 // StringPicture
 class StringPictureBase : public PictureBase {
-public:
-	StringPictureBase();
-	~StringPictureBase();
 private:
+	std::vector<std::string> data;
+	StringPictureBase(const std::vector<std::string>& v) : data(v) {}
 
+	wd_sz width() const;
+	ht_sz height() const;
+	void display(std::ostream&, ht_sz, bool) const;
 };
 
 // FramePicture
 class FramePictureBase : public PictureBase {
-public:
-	FramePictureBase();
-	~FramePictureBase();
-private:
+	Ptr<PictureBase> p;
+	FramePictureBase(const Ptr<PictureBase>& pic) : p(pic) {}
 
+	wd_sz width() const;
+	ht_sz height() const;
+	void display(std::ostream&, ht_sz, bool) const;
 };
 
 // HCatPicture
 class HCatPictureBase : public PictureBase {
-public:
-	HCatPictureBase();
-	~HCatPictureBase();
 private:
+	Ptr<PictureBase> left, right;
+	HCatPictureBase(const Ptr<PictureBase>& l, const Ptr<PictureBase>& r) :
+		left(l), right(r) {}
+
+	wd_sz width() const;
+	ht_sz height() const;
+	void display(std::ostream&, ht_sz, bool) const;
 
 };
 
 // VCatPicture
 class VCatPictureBase : public PictureBase {
-public:
-	VCatPictureBase();
-	~VCatPictureBase();
 private:
+	Ptr<PictureBase> top, bottom;
+	VCatPictureBase(const Ptr<PictureBase>& t, const Ptr<PictureBase>& b) :
+		top(t), bottom(b) {}
 
+	wd_sz width() const;
+	ht_sz height() const;
+	void display(std::ostream&, ht_sz, bool) const;
 };
 
 
